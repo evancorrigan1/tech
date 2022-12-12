6e4da11e430bafdaa1ef6bc582f9dec29fff6e4d
evancorrigan1 <120377803+evancorrigan1@users.noreply.github.com>
2022-12-12 11:03:53 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onSound(DetectedSound.Loud, function () {
+    Kitronik_Robotics_Board.allOff()
+})
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     for (let index = 0; index < 4; index++) {
         Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 30)
@@ -7,6 +10,7 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
         Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 30)
         basic.pause(2500)
         Kitronik_Robotics_Board.motorOff(Kitronik_Robotics_Board.Motors.Motor1)
-        basic.pause(100)
     }
 })
+led.plot(2, 2)
+input.setSoundThreshold(SoundThreshold.Loud, 128)
